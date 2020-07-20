52763c9e063f646eab6724e1d42edf4e8c85f060
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-07-20 12:07:53 -0300

    Create 1 - CREATE.sql

---
+++ Querying data/1 - CREATE.sql
@@ -0,0 +1,6 @@
+INSERT INTO groceries VALUES (1, "Bananas", 4, 7);
+INSERT INTO groceries VALUES(2, "Peanut Butter", 1, 2);
+INSERT INTO groceries VALUES(3, "Dark Chocolate Bars", 2, 2);
+INSERT INTO groceries VALUES(4, "Ice cream", 1, 12);
+INSERT INTO groceries VALUES(5, "Cherries", 6, 2);
+INSERT INTO groceries VALUES(6, "Chocolate syrup", 1, 4);
